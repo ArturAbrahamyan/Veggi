a3ec4c32096d83564a9c0f4061775a65b794bdda
Artur <37485713+ArturAbrahamyan@users.noreply.github.com>
2019-01-17 19:36:19 +0400

    Update view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,12 @@
 var app= angular.module('myApp.view1', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/', {
 
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
 
-  });
+  }).otherwise({redirectTo: '/view1'});
 }]);
 
 app.controller('View1Ctrl', function($scope) {
